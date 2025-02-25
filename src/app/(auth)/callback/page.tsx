327c83ee10a290694a8dfdb0c680ddd065460411
unknown <dhanya13md@gmail.com>
2025-02-26 00:35:06 +0530

    basic skeleton structure with prisma

---
+++ src/app/(auth)/callback/page.tsx
@@ -0,0 +1,21 @@
+import { onAuthentiatedUser } from '@/actions/user'
+import { redirect } from 'next/navigation'
+import React from 'react'
+
+const AuthCallbackPage = async () => {
+    const auth = await onAuthentiatedUser()
+
+    if( auth.status === 200 || auth.status === 201) {
+        redirect('/dashboard')
+    }else if (
+        auth.status === 403 || 
+        auth.status === 400 || 
+        auth.status === 500
+    ) {
+        redirect('/sign-in')
+    }
+
+}
+
+export default AuthCallbackPage
+ 
