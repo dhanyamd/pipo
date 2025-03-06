ff38e399682d7fe82284e1a295e863c9e39298de
unknown <dhanya13md@gmail.com>
2025-03-06 23:32:16 +0530

    setup pages

--- src/components/globals/upper-info-bar/new-project-button.tsx
+++ src/components/globals/upper-info-bar/new-project-button.tsx
@@ -3,13 +3,13 @@ import { Button } from '@/components/ui/button'
 import { User } from '@prisma/client'
 import { Plus } from 'lucide-react'
 import { useRouter } from 'next/navigation'
-import React, { use } from 'react'
+import React from 'react'
 
 const NewProjectButton = ({user} : {user: User}) => {
     const router = useRouter()
   return (
     <Button
-    size={'lg'}
+    onClick={() => router.push('/create-page')}
     className='rounded-lg font-semibold'
     disabled={!user.subscription}
     >
